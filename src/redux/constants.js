23df9aa7d65dbc278489fbb05faa2bbdfadc38ac
MikhaelEmpi <d.mikhaelempi@gmail.com>
2024-07-05 10:48:36 +0700

    fix constants.js base url

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,7 +1,7 @@
-export const BASE_URL = "";
-export const USERS_URL = "/api/users";
-export const CATEGORY_URL = "/api/category";
-export const PRODUCT_URL = "/api/products";
-export const UPLOAD_URL = "/api/upload";
-export const ORDERS_URL = "/api/orders";
-export const MIDTRANS_URL = "/api/config/midtrans";
+export const BASE_URL = "https://ecommercekue-backend-production.up.railway.app";
+export const USERS_URL = `${BASE_URL}/api/users`;
+export const CATEGORY_URL = `${BASE_URL}/api/category`;
+export const PRODUCT_URL = `${BASE_URL}/api/products`;
+export const UPLOAD_URL = `${BASE_URL}/api/upload`;
+export const ORDERS_URL = `${BASE_URL}/api/orders`;
+export const MIDTRANS_URL = `${BASE_URL}/api/config/midtrans`;
