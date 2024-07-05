7267b3841d47322618e0bf8b888cc80b97610732
MikhaelEmpi <d.mikhaelempi@gmail.com>
2024-07-05 10:36:48 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
+      "/api/": "https://ecommercekue-backend-production.up.railway.app",
+      "/uploads/": "https://ecommercekue-backend-production.up.railway.app",
     }
   }
 })
